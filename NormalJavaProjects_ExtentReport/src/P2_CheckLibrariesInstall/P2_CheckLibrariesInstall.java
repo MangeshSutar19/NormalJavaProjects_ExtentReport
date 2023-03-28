e247882463af6098efbb5fa6729801ade2fe9c68
MangeshSutar19 <mangeshsutar1901@outtlok.com>
2023-03-29 05:22:23 +0530

    Normaljava project_ExtentReport

---
+++ NormalJavaProjects_ExtentReport/src/P2_CheckLibrariesInstall/P2_CheckLibrariesInstall.java
@@ -0,0 +1,17 @@
+package P2_CheckLibrariesInstall;
+
+import com.aventstack.extentreports.ExtentReports; 
+
+//if we are not getting import statement that means our libraries are not installed properly
+
+public class P2_CheckLibrariesInstall {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ExtentReports reports = new ExtentReports();
+		
+
+	}
+
+}
